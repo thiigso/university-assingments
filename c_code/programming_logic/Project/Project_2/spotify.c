b5eb6caceacf24a5ac77c53672f46e658e9ef0a2
Thiago Gabriel <thiagogabriel.tgso@gmail.com>
2025-12-17 22:57:10 -0300

    Prog_log project: Start last two functions and add song verification

--- c_code/programming_logic/Project/Project_2/spotify.c
+++ c_code/programming_logic/Project/Project_2/spotify.c
@@ -25,6 +25,9 @@ void StripNewLine(char vector[]){
 
 void AddSongs(playlist vector[]){
 
+    char song_name_temp[CHAR], artist_name_temp[CHAR];
+    int launch_year_temp, duration_temp;
+
     if(structQtd >= MAX){
         printf("A lista ja esta cheia, nao e possivel adicionar mais musicas\n");
         return;
@@ -32,17 +35,30 @@ void AddSongs(playlist vector[]){
     else{
 
     printf("Digite o nome da musica: ");
-    fgets(vector[structQtd].song_name, sizeof(vector[structQtd].song_name),stdin);
-    StripNewLine(vector[structQtd].song_name);
+    fgets(song_name_temp, sizeof(song_name_temp),stdin);
+    StripNewLine(song_name_temp);
     printf("Digite o nome do artista: ");
-    fgets(vector[structQtd].artist_name, sizeof(vector[structQtd].artist_name),stdin);
-    StripNewLine(vector[structQtd].artist_name);
+    fgets(artist_name_temp, sizeof(artist_name_temp),stdin);
+    StripNewLine(artist_name_temp);
     printf("Digite o ano de lancamento: ");
-    scanf("%d",&vector[structQtd].launch_year); 
+    scanf("%d",&launch_year_temp); 
     ClearBuffer();
     printf("Digite a duracao da musica: ");
-    scanf("%d",&vector[structQtd].duration);
+    scanf("%d",&duration_temp);
     ClearBuffer();
+
+
+    for(int i=0; i<structQtd; i++){
+        if(!strcmp(song_name_temp, vector[i].song_name) && !strcmp(artist_name_temp, vector[i].artist_name)){
+            printf("Essa musica ja existe na playlist, por favor escolha outra.");
+            return;
+        }
+    }
+    
+    strcpy(vector[structQtd].song_name,song_name_temp);
+    strcpy(vector[structQtd].artist_name,artist_name_temp);
+    vector[structQtd].launch_year = launch_year_temp;
+    vector[structQtd].duration = duration_temp;
 
     structQtd++;
         
@@ -52,29 +68,33 @@ void AddSongs(playlist vector[]){
 void ListSongs(playlist vector[]){
 
     for(int i=0; i<structQtd; i++){
-        printf("Musica: %d\nNome: %s\nArtista: %s\nAno: %d\nDuração: %d\n\n",i+1, vector[i].song_name, vector[i].artist_name, vector[i].launch_year, vector[i].duration);
+        printf("Musica: %d\nNome: %s\nArtista: %s\nAno: %d\nDuracao: %d\n\n",i+1, vector[i].song_name, vector[i].artist_name, vector[i].launch_year, vector[i].duration);
     }
 
 }
 
-void SearchSongs(playlist vector[]){
+int SearchSongs(playlist vector[]){
     int searchQtd = 0;
     char nomeTemp[50];
     
     printf("Digite a musica a ser procurado na lista: ");
-    ClearBuffer();
     fgets(nomeTemp, sizeof(nomeTemp),stdin);
     StripNewLine(nomeTemp);
 
     for(int i=0; i<structQtd; i++){
 
         if(!strcmp(nomeTemp, vector[i].song_name)){
-        printf("Musica: %d\nNome: %s\nArtista: %s\nAno: %d\nDuração: %d\n\n",i+1, vector[i].song_name, vector[i].artist_name, vector[i].launch_year, vector[i].duration);
+        printf("Musica: %d\nNome: %s\nArtista: %s\nAno: %d\nDuracao: %d\n\n",i+1, vector[i].song_name, vector[i].artist_name, vector[i].launch_year, vector[i].duration);
             searchQtd++;
         }
     }
 
     if(searchQtd == 0) printf("\nA musica descrita nao foi encontrado na lista.\n");
+
+}
+
+void DeleteSong(playlist vector[]){
+
 
 }
 
